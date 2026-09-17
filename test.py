fd21d711f001d8ada596804cd1079bafe59918d1
Krish Kapoor <kapoorkrish191@gmail.com>
2026-09-17 22:45:06 +0530

    test.py

--- test.py
+++ test.py
@@ -11,21 +11,8 @@ def plot_results(results):
         plt.title('YOLOv8 Detection Results')
         plt.axis('off')
         plt.show()
-def get_detect_folders(main_path):
-    folder_list = []
 
-    for item in os.listdir(main_path):
-        full_path = os.path.join(main_path, item)
 
-        if os.path.isdir(full_path):
-            folder_list.append({
-                "name": item,
-                "path": full_path,
-                "created_time": os.path.getctime(full_path)
-            })
-def get_detect_folders(base_path):
-    full_path = os.path.join(base_path, "detect")
-     
 
     return folder_list            
     
